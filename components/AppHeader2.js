e67cfd79e555fb1600c6ddbee34275e61fcf44bd
DeepaDidharia <79315183+DeepaDidharia@users.noreply.github.com>
2022-02-18 13:28:14 +0530

    components

---
+++ components/AppHeader2.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class AppHeader extends React.Component{
+  render(){
+    return(
+      <View style= {styles.textContainer}>
+        <Text style={styles.text}>  RESULTS </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textContainer:{
+    backgroundColor: 'purple',
+    height: 60,
+  },
+  
+  text:{
+    color: 'lightgreen',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default AppHeader;
